07ac4a2b870e114baf9ca9db14a9dcd5ed658d68
inagaki <inagaki-hiroyuki@mgt-net.com>
2018-08-23 17:20:24 +0900

    Initial Commit

---
+++ counterAppQuestion/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  counterAppQuestion
+//
+//  Created by user on 2018/08/23.
+//  Copyright © 2018年 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    var count:Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        label.text = String(count)
+        label.layer.cornerRadius = 20
+        self.label.layer.masksToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // カウント加算メソッド
+    @IBAction func countUp(_ button:UIButton) {
+        count += 1
+        label.text = String(count)
+    }
+    
+    // カウントクリアメソッド
+    @IBAction func countClear(_ button:UIButton) {
+        count = 0
+        label.text = String(count)
+    }
+
+}
+
