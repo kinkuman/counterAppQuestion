0223f7f301f3232750825faf8149dc70ac47780c
inagaki <inagaki-hiroyuki@mgt-net.com>
2018-08-23 18:09:47 +0900

    first

--- counterAppQuestion/ViewController.swift
+++ counterAppQuestion/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     var count:Int = 0
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
